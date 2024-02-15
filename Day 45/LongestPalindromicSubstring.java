f6cf936ef5a3c0408f59e4c3a19b276d98df48be
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2024-02-15 10:48:31 +0530

    Update LongestPalindromicSubstring.java

--- Day 45/LongestPalindromicSubstring.java
+++ Day 45/LongestPalindromicSubstring.java
@@ -12,7 +12,38 @@ Input: s = "cbbd"
 Output: "bb"
 */
 class LongestPalindromicSubstring{
+	static String findLongestPalindromicSubstring(String s){
+		if(s.length()<=1){
+			return s;
+		}
+		String maxStr=s.substring(0,1);
+		for(int i=0;i<s.length();i++){
+			String odd=expandFromcenter(s,i,i);
+			String even=expandFromcenter(s,i,i+1);
+
+			if(odd.length()>maxStr.length()){
+				maxStr=odd;
+			}
+			if(even.length()>maxStr.length()){
+				maxStr=even;
+			}
+		}
+		return maxStr;
+	}
+	static String expandFromcenter(String s, int left,int right){
+		while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+			left--;
+			right++;
+		}
+
+		return s.substring(left+1,right);
+	}
 	public static void main(String[] args) {
-		
+		String s="babad";
+		System.out.println(findLongestPalindromicSubstring(s));
 	}
 }
+/*
+OUTPUT
+bab
+*/
