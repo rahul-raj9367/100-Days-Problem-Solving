c2fbc3949381949425f3d629f07c98e97b5a97bb
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2024-03-28 20:17:41 +0530

    Add files via upload

---
+++ Day 88/CheckifaStringIsanAcronymofWords.java
@@ -0,0 +1,57 @@
+/*
+Given an array of strings words and a string s, determine if s is an acronym of words.
+
+The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+Return true if s is an acronym of words, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: words = ["alice","bob","charlie"], s = "abc"
+Output: true
+Explanation: The first character in the words "alice", "bob", and "charlie" are 'a', 'b', and 'c', respectively. Hence, s = "abc" is the acronym. 
+Example 2:
+
+Input: words = ["an","apple"], s = "a"
+Output: false
+Explanation: The first character in the words "an" and "apple" are 'a' and 'a', respectively. 
+The acronym formed by concatenating these characters is "aa". 
+Hence, s = "a" is not the acronym.
+Example 3:
+
+Input: words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+Output: true
+Explanation: By concatenating the first character of the words in the array, we get the string "ngguoy". 
+Hence, s = "ngguoy" is the acronym.
+*/
+import java.util.*;
+class CheckifaStringIsanAcronymofWords{
+	static boolean findCheckifaStringIsanAcronymofWords(List<String> li,String s){
+		int i=0;
+		int n=s.length();
+
+		if(li.size()!=n) return false; 
+		for(String word:li){
+			if(word.charAt(0)!=s.charAt(i++)) return false;
+		}
+
+		return true;
+	}
+	public static void main(String[] args) {
+		List<String> li=new ArrayList<>();
+
+		li.add("alice");
+		li.add("bob");
+		li.add("charlie");
+
+		String s="abc";
+
+		System.out.println(findCheckifaStringIsanAcronymofWords(li,s));
+	}
+}
+/*
+OUTPUT
+true
+*/
